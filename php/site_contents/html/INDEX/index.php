f01d9ea1770d466e51758339679edc2a109b73f0
Daniel White <drwhite@wallstreak.com.au>
2018-03-08 02:10:33 +1100

    Formatting
    Formatting

--- php/site_contents/html/INDEX/index.php
+++ php/site_contents/html/INDEX/index.php
@@ -1,7 +1,7 @@
 <?php namespace WEBSITE_NAMESPACE { class HTML_INDEX { function __construct() { } public function HTML_Index() { } } ?>
 
 <div class='welcome'>
-<p>Welcome to your fresh new PHP Template!<p>
+    <p>Welcome to your fresh new PHP Template!<p>
 </div>
 
 <?php } ?>
