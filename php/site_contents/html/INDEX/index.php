bf9130bc2e64424a044530ea3cac0ef92c018d59
Daniel White <drwhite@wallstreak.com.au>
2018-03-08 02:10:20 +1100

    Fancy new index.php
    Fixed index.php in terms of error codes rlating to html code being outside of namespace, also made it look prettier.

--- php/site_contents/html/INDEX/index.php
+++ php/site_contents/html/INDEX/index.php
@@ -1,7 +1,7 @@
 <?php namespace WEBSITE_NAMESPACE { class HTML_INDEX { function __construct() { } public function HTML_Index() { } } ?>
 
 <div class='welcome'>
-Welcome to your fresh new PHP Template!
+<p>Welcome to your fresh new PHP Template!<p>
 </div>
 
 <?php } ?>
