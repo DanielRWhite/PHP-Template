1073461305b9409cbd499b684a7f9ced2b9691e1
Daniel White <drwhite@wallstreak.com.au>
2018-03-08 03:03:44 +1100

    formatting
    formatting

--- php/site_contents/html/404/index.php
+++ php/site_contents/html/404/index.php
@@ -1,13 +1,4 @@
-<?php
-namespace WEBSITE_NAMESPACE {
-
-    class SiteNotFound {
-
-        function         __construct() { }
-        public function SiteNotFound() { }
-    }
-
-?>
+<?php namespace WEBSITE_NAMESPACE { class SiteNotFound { function __construct() { } public function SiteNotFound() { } } ?>
 
 <div class='welcome'>
     <p>404! That page can't be found, why not go back to the <a href='/'>Homepage</a>?</p>
